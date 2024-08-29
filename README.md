5ef3205a38bc1f48d2196e5ba8c2588e9bbc7c49
etherwrangler <7719442+etherwrangler@users.noreply.github.com>
2024-08-29 14:57:47 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Setup for MacOS
+
+Install chezmoi (via homebrew)
+```shell
+brew install chezmoi
+```
+more stuff soon
---
+++ chzhome/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+config.color_scheme = 'Catppuccin Macchiato'
+config.font_size = 16
+config.font = 
+    wezterm.font('JetBrainsMono Nerd Font', {weight = 'Bold', italic = false})
+
+config.hide_tab_bar_if_only_one_tab = true
+config.hide_mouse_cursor_when_typing = true
+config.default_cursor_style = 'SteadyUnderline'
+-- config.window_decorations = 'RESIZE'
+
+config.initial_cols = 100
+config.initial_rows = 30
+
+return config
