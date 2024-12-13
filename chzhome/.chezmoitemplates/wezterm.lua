ec2adef2dd8a2f64a50cb8b5820c1fabdc4fc263
etherwrangler <7719442+etherwrangler@users.noreply.github.com>
2024-12-13 08:18:19 -0600

    make new windows spawn in home directory

--- chzhome/.chezmoitemplates/wezterm.lua
+++ chzhome/.chezmoitemplates/wezterm.lua
@@ -12,6 +12,10 @@ config.hide_mouse_cursor_when_typing = true
 config.default_cursor_style = 'SteadyUnderline'
 -- config.window_decorations = 'RESIZE'
 
+config.keys = {
+    { key = 'n', mods = 'CMD', action = wezterm.action.SpawnCommandInNewWindow { cwd=wezterm.home_dir } },
+}
+
 config.initial_cols = 100
 config.initial_rows = 30
 
