2b79fe7961ec41708c2f6650d28a2862c72180f5
etherwrangler <7719442+etherwrangler@users.noreply.github.com>
2026-02-12 13:08:02 -0600

    clean up wezterm config

--- chzhome/.chezmoitemplates/wezterm.lua
+++ chzhome/.chezmoitemplates/wezterm.lua
@@ -1,22 +1,21 @@
 local wezterm = require 'wezterm'
 
-local config = {}
-
-config.color_scheme = 'Catppuccin Macchiato'
-config.font_size = 16
-config.font = 
-    wezterm.font('JetBrainsMono Nerd Font', {weight = 'Bold', italic = false})
-
-config.hide_tab_bar_if_only_one_tab = true
-config.hide_mouse_cursor_when_typing = true
-config.default_cursor_style = 'SteadyUnderline'
--- config.window_decorations = 'RESIZE'
-
-config.keys = {
-    { key = 'n', mods = 'CMD', action = wezterm.action.SpawnCommandInNewWindow { cwd=wezterm.home_dir } },
+return {
+    adjust_window_size_when_changing_font_size = false,
+    initial_cols = 100,
+    initial_rows = 30,
+    color_scheme = 'Catppuccin Macchiato',
+    font_size = 16,
+    font = wezterm.font('JetBrainsMono Nerd Font'),
+    hide_tab_bar_if_only_one_tab = true,
+    hide_mouse_cursor_when_typing = true,
+    default_cursor_style = 'SteadyUnderline',
+    --window_decorations = 'RESIZE',
+    keys = {
+        {
+            key = 'n',
+            mods = 'CMD',
+            action = wezterm.action.SpawnCommandInNewWindow { cwd=wezterm.home_dir },
+        },
+    },
 }
-
-config.initial_cols = 100
-config.initial_rows = 30
-
-return config
