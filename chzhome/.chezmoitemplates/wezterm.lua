eb39099f458fafff3f2fae828736c4e376cb4c51
etherwrangler <7719442+etherwrangler@users.noreply.github.com>
2026-05-20 18:06:26 -0500

    refactor: use wezterm config builder

--- chzhome/.chezmoitemplates/wezterm.lua
+++ chzhome/.chezmoitemplates/wezterm.lua
@@ -1,21 +1,23 @@
 local wezterm = require 'wezterm'
 
-return {
-    adjust_window_size_when_changing_font_size = false,
-    initial_cols = 100,
-    initial_rows = 30,
-    color_scheme = 'Catppuccin Mocha',
-    font_size = 16,
-    font = wezterm.font('JetBrainsMono Nerd Font'),
-    hide_tab_bar_if_only_one_tab = true,
-    hide_mouse_cursor_when_typing = true,
-    default_cursor_style = 'SteadyUnderline',
-    --window_decorations = 'RESIZE',
-    keys = {
-        {
-            key = 'n',
-            mods = 'CMD',
-            action = wezterm.action.SpawnCommandInNewWindow { cwd=wezterm.home_dir },
-        },
+local config = wezterm.config_builder()
+
+config.adjust_window_size_when_changing_font_size = false
+config.initial_cols = 100
+config.initial_rows = 30
+config.color_scheme = 'Catppuccin Mocha'
+config.font_size = 16
+config.font = wezterm.font('JetBrainsMono Nerd Font')
+config.hide_tab_bar_if_only_one_tab = true
+config.hide_mouse_cursor_when_typing = true
+config.default_cursor_style = 'SteadyUnderline'
+--config.window_decorations = 'RESIZE'
+config.keys = {
+    {
+        key = 'n',
+        mods = 'CMD',
+        action = wezterm.action.SpawnCommandInNewWindow { cwd = wezterm.home_dir },
     },
 }
+
+return config
